14144412a3d5af4e1277b2c7d05460e2cf78aacb
shaleentn <shaleentn@gmail.com>
2024-03-20 15:11:23 +0300

    images and links

--- Functions.py
+++ Functions.py
@@ -1,15 +1,17 @@
-def calculate_discount(price, discount_percent):
+#def calculate_discount(price, discount_percent):
    
           
-        if discount_percent>=20:
-            new_price=((100-discount_percent)/100) * price
-            print(new_price)
+     #   if discount_percent>=20:
+    #        new_price=((100-discount_percent)/100) * price
+   #         print(new_price)
 
-        else:
-            print(price)
-a=int(input("Enter price: "))
-b=int(input("Enter discount in percentage: "))
+  #      else:
+ #           print(price)
+#a=int(input("Enter price: "))
+#b=int(input("Enter discount in percentage: "))
 
-calculate_discount(a,b)            
-        
-    
+#calculate_discount(a,b)            
+
+a,b=0,1
+print(a)
+print(b)
