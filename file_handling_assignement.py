46862dd440c8f5827f6d7cf85aef6888c63eaeed
Shaleen Atieno <135377616+shaleentn@users.noreply.github.com>
2024-05-06 12:29:59 +0300

    Update file_handling_assignement.py

--- file_handling_assignement.py
+++ file_handling_assignement.py
@@ -1,25 +1,25 @@
-# file=open("./PLP/my_file.txt", "w")
-# file.write("Hello there\n")
-# file.write("My name is Shaleen Atieno\n")
-# file.write("I am a student\n")
-# file.write("\'Singing\' is my hobby\n")
-# file.write("My registration Number is S13/0897/22\n")
+file=open("./PLP/my_file.txt", "w")
+file.write("Hello there\n")
+file.write("My name is Shaleen Atieno\n")
+file.write("I am a student\n")
+file.write("\'Singing\' is my hobby\n")
+file.write("My registration Number is S13/0897/22\n")
 
 
-# file1=open("./PLP/my_file.txt", "r")
-# print(file1.read())
-# print(file1.readline())
-# print(file1.readlines())
-# file1.close()
+file1=open("./PLP/my_file.txt", "r")
+print(file1.read())
+print(file1.readline())
+print(file1.readlines())
+file1.close()
 
 
 
-# file2=open("./PLP/my_file.txt", "a")
+file2=open("./PLP/my_file.txt", "a")
 
 
-# file2.write("PLp academy is the best\n")
-# file2.write("Can't wait to build my own project")
-# file2.close()
+file2.write("PLp academy is the best\n")
+file2.write("Can't wait to build my own project")
+file2.close()
 
 
 
